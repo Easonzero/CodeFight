cbfe7b3cf9a9cdc3093bc1e2055cd6f35db69595
eason <451114984@qq.com>
2016-12-02 11:37:01 +0800

    去除编译文件

--- app/game/element/stage/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by eason on 16-9-22.
- */
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./stage.container'));
-//# sourceMappingURL=index.js.map
