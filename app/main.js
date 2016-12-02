6116ff2fb20012e9bed9a2bd24cce39fe75cd2e4
eason <451114984@qq.com>
2016-12-02 11:16:08 +0800

    去除编译文件，添加git忽略

--- app/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Created by eason on 16-9-19.
- */
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app.module');
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
-//# sourceMappingURL=main.js.map
